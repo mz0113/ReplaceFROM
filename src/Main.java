01ec6a3e3d66e7f5460879070d693b278a039979
Administrator <1172295848@qq.com>
2018-09-23 15:14:11 +0800

    两次转换，第一次转为一行并添加字符串，第二次去除+号

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,18 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
+        replaceFirst();
+        replaceSecond();
+    }
 
-        final String PATH = "E:/新建文件夹/";
-        final String CONVERTPATH = "E:/转换后/";
-        final String FROM = "FROM";
-        final String ADD = ",PbocFeeCrncyCode FROM";
+    /**
+     * 替换+号
+     */
+    private static void replaceSecond() {
+        final String PATH = "E:/一次转换/";
+        final String CONVERTPATH = "E:/二次转换/";
+        final String FROM = "+";
+        final String ADD = " ";
         File file = new File(PATH);
         if(file.isDirectory()){
             File[] fArray = file.listFiles();
@@ -19,8 +26,8 @@ public class Main {
                     for(String temp=null;(temp = bufferedReader.readLine())!=null;temp=null){
                         temp.toUpperCase();
                         if(temp.indexOf(FROM)!=-1){
-                          replaceStr = temp.substring(temp.indexOf(FROM),temp.indexOf(FROM)+4);
-                          temp = temp.replace(replaceStr,ADD);
+                            replaceStr = temp.substring(temp.indexOf(FROM),temp.indexOf(FROM)+1);
+                            temp = temp.replace(replaceStr,ADD);
                         }
                         strBuffer.append(temp);
                     }
@@ -42,5 +49,51 @@ public class Main {
             }
             System.out.println("---------- 所有文件操作完毕 ----------");
         }
+    }
+
+
+    /**
+     * 替换FROM，多行内容也会自动转换为单行。append()时候没有加换行符
+     */
+    static void replaceFirst(){
+
+       final String PATH = "E:/新建文件夹/";
+       final String CONVERTPATH = "E:/一次转换/";
+       final String FROM = "FROM";
+       final String ADD = ",PbocFeeCrncyCode FROM";
+       File file = new File(PATH);
+       if(file.isDirectory()){
+           File[] fArray = file.listFiles();
+           for (File f:fArray) {
+               try {
+                   String replaceStr = null;
+                   StringBuffer strBuffer = new StringBuffer();
+                   BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+                   for(String temp=null;(temp = bufferedReader.readLine())!=null;temp=null){
+                       temp.toUpperCase();
+                       if(temp.indexOf(FROM)!=-1){
+                           replaceStr = temp.substring(temp.indexOf(FROM),temp.indexOf(FROM)+4);
+                           temp = temp.replace(replaceStr,ADD);
+                       }
+                       strBuffer.append(temp);
+                   }
+                   bufferedReader.close();
+                   PrintWriter printWriter = new PrintWriter(CONVERTPATH+f.getName());
+                   printWriter.write(strBuffer.toString());
+                   printWriter.flush();
+                   printWriter.close();
+                   System.out.println(f.getName()+"文件操作成功!\n");
+               } catch (FileNotFoundException e) {
+                   System.out.println(e.getMessage());
+                   e.printStackTrace();
+                   return;
+               } catch (IOException e) {
+                   System.out.println(e.getMessage());
+                   e.printStackTrace();
+                   return;
+               }
+           }
+           System.out.println("---------- 所有文件操作完毕 ----------");
+       }
     }
 }
