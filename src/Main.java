0250828518c5efe006d802742a5396b9d45d94dd
Administrator <1172295848@qq.com>
2018-09-28 22:52:48 +0800

    两次转换，第一次转为一行并添加字符串，第二次去除+号

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        replaceFirst();
-        replaceSecond();
+     //   replaceFirst();
+    //    replaceSecond();
+        extractFiles(true);
     }
 
     /**
@@ -95,5 +96,57 @@ public class Main {
            }
            System.out.println("---------- 所有文件操作完毕 ----------");
        }
+    }
+
+
+
+    private static void extractFiles(boolean isCopy){
+        final String[] fileNames = new String[]{
+                "FD48.txt",
+                "FD58.txt",
+        };
+        final String srcDirPATH = "E:/提取源文件夹/";
+        final String desDirPATH = "E:/提取输出文件夹/";
+
+        for (String fileName:fileNames) {
+            String srcFilePath = srcDirPATH + fileName;
+            File srcFile = new File(srcFilePath);
+            if (!srcFile.exists()) {
+                System.out.println(",/////////" + fileName + "is not exists!///////////");
+                return;
+            }
+            File dirFile = new File(desDirPATH);//获取文件夹路径
+            if (!dirFile.exists()) {//判断文件夹是否创建，没有创建则创建新文件夹
+                dirFile.mkdirs();
+            }
+
+
+            if (!isCopy) {
+                if (srcFile.renameTo(new File(desDirPATH + srcFile.getName()))) {
+                    System.out.println(srcFile.getName()+" ,File is moved successful!");
+                } else {
+                    System.out.println(",----------------"+srcFile.getName()+" ,is failed to move!---------------------");
+                }
+            } else {
+                try {
+                    FileInputStream fis = new FileInputStream(srcFile);
+                    FileOutputStream fos = new FileOutputStream(new File(desDirPATH + srcFile.getName()));
+                    byte[] b = new byte[1024];
+                    int len;
+                    while ((len = fis.read(b)) != -1) {
+                        fos.write(b, 0, len);
+                    }
+                    fos.close();
+                    fis.close();
+                    System.out.println(srcFile.getName() + " ,is copyed successful!");
+                } catch (FileNotFoundException e) {
+                    System.out.println(",-------------\"+srcFile.getName()+\" ,is failed to copy!---------------------");
+                    e.printStackTrace();
+                    return;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
